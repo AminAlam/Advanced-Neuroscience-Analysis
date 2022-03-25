abafa557bd56b0d279871dd1f2cd6249aa471fec
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-03-25 21:22:05 +0430

    r_s, r_sc, and distance between neurones are calculated

--- Noise_Correlation/Answer/main.m
+++ Noise_Correlation/Answer/main.m
@@ -1,4 +1,3 @@
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% part 1
 clc
 clear
 close all
@@ -11,7 +10,10 @@ for monk_no = 1:3
     data = load("data/spikes_gratings/infos_monkey"+num2str(monk_no)+".mat");
     monk_elec_info{monk_no} = data.keepNeurons_mat_tmp;
 end
-%% finding maximum activity
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% part 1
+% finding maximum activity
+clc
+close all
 activity_max = zeros(3, 12);
 
 for monk_no = 1:3
@@ -36,7 +38,7 @@ for grating_no = 1:12
     data = monk_data{monk_no};
     elec_info1 = monk_elec_info{monk_no};
     max_activity_neuron = activity_max(monk_no, grating_no);
-    activity = data(grating_no).mean_FRs;
+    activity = data(grating_no).mean_FRs/2e-2;
     color = [0, 0, 0];
     lineWidth = 2;
     plot(0:20:980, activity(max_activity_neuron, :), 'color', color, 'LineWidth', lineWidth)
@@ -45,7 +47,7 @@ for grating_no = 1:12
     data = monk_data{monk_no};
     elec_info2 = monk_elec_info{monk_no};
     max_activity_neuron = activity_max(monk_no, grating_no);
-    activity = data(grating_no).mean_FRs;
+    activity = data(grating_no).mean_FRs/2e-2;
     color = [1, 0, 0];
     lineWidth = 2;
     plot(0:20:980, activity(max_activity_neuron, :), 'color', color, 'LineWidth', lineWidth)
@@ -54,7 +56,7 @@ for grating_no = 1:12
     data = monk_data{monk_no};
     elec_info3 = monk_elec_info{monk_no};
     max_activity_neuron = activity_max(monk_no, grating_no);
-    activity = data(grating_no).mean_FRs;
+    activity = data(grating_no).mean_FRs/2e-2;
     color = [0, 0, 1];
     lineWidth = 2;
     plot(0:20:980, activity(max_activity_neuron, :), 'color', color, 'LineWidth', lineWidth)
@@ -70,7 +72,7 @@ for grating_no = 1:12
         lineWidth = 1;
         data = monk_data{monk_no};
         max_activity_neuron = activity_max(monk_no, grating_no);
-        activity = data(grating_no).mean_FRs;
+        activity = data(grating_no).mean_FRs/2e-2;
         plot(0:20:980, mean(activity, 1), 'color', color, 'LineWidth', lineWidth)
     end
     
@@ -78,8 +80,9 @@ for grating_no = 1:12
         "Neuron No."+num2str(elec_info2(activity_max(2, grating_no)))+" | Monk No.2", ...
         "Neuron No."+num2str(elec_info3(activity_max(3, grating_no)))+" | Monk No.3", ...
         "Avg | Monk No.1", "Avg | Monk No.2", "Avg | Monk No.3")
-    
-    title("PSTH Plot for grating = "+num2str((grating_no-1)*30))
+    xlabel('Time (ms)')
+    ylabel('Firing Rate (Hz)')
+    title("PSTH Plot for orientation = "+num2str((grating_no-1)*30))
 end
 
 %% plotting tuning curve
@@ -93,14 +96,14 @@ for monk_no = 1:3
     neurons = unique(activity_max(monk_no, :));
     neurons = neurons([1,2]);
     activity_mat = zeros(length(neurons), 12);
-    elec_info = monk_elec_info{monk_no};
+    neurons_info = monk_elec_info{monk_no};
     for neuron_no = neurons
         for grating_no = 1:12
             data = monk_data{monk_no};
             data = data(grating_no).mean_FRs;
             activity = data(neuron_no, :);
             activity = mean(activity, 2);
-            activity_mat(neuron_indx, grating_no) = activity;    
+            activity_mat(neuron_indx, grating_no) = activity/2e-2;    
         end
         if neuron_indx == 1
             color = 'k';
@@ -116,12 +119,13 @@ for monk_no = 1:3
         data = monk_data{monk_no};
         data = data(grating_no).mean_FRs;
         activity = mean(data, 'all');
-        activity_mat(1, grating_no) = activity;    
+        activity_mat(1, grating_no) = activity/2e-2;    
     end
     plot(0:30:330, activity_mat(1,:), '--m', 'LineWidth', 1)
-    
-    legend("Neuron No."+num2str(elec_info(neurons(1))), ...
-        "Neuron No."+num2str(elec_info(neurons(2))), ...
+    xlabel('Orientation')
+    ylabel('Firing Rate (Hz)')
+    legend("Neuron No."+num2str(neurons_info(neurons(1))), ...
+        "Neuron No."+num2str(neurons_info(neurons(2))), ...
         "Avg")
     title("Tuning Curves for Monk No."+num2str(monk_no))
 end
@@ -132,7 +136,9 @@ clc
 close all
 for monk_no = 1:3
     data = monk_data{monk_no};
-    elec_info = monk_elec_info{monk_no};
+    channels = monk_data_stuff{monk_no}.CHANNELS;
+    loc_map = monk_data_stuff{monk_no}.MAP;
+    neurons_info = monk_elec_info{monk_no};
     neurons = 1:size(data(1).mean_FRs, 1);
     preferred_grating_mat = zeros(1, length(neurons));
     preferred_grating_mat_tmp = zeros(12, length(neurons));
@@ -146,15 +152,18 @@ for monk_no = 1:3
         [row, ~] = find(preferred_grating_mat_tmp(:, neuron_no)==max(preferred_grating_mat_tmp(:, neuron_no)));
         preferred_grating_mat(neuron_no) = row(1);
     end
-    loc_map = monk_data_stuff{monk_no}.MAP;
     grating_map = zeros(size(loc_map, 1), size(loc_map, 2))*nan;
     for i = 1:size(loc_map, 1)
         for j = 1:size(loc_map, 2)
             elec_no = loc_map(i, j);
             if ~isnan(elec_no)
-                [row, ~] = find(elec_info==elec_no);
+                [row, ~] = find(channels(:,1)==elec_no);
                 if ~isempty(row)
-                    grating_map(i, j) = (preferred_grating_mat(row)-1)*30;
+                    target_neuron = row(1);               
+                    [row, ~] = find(neurons_info==target_neuron);
+                    if ~isempty(row)
+                        grating_map(i, j) = (preferred_grating_mat(row)-1)*30;
+                    end
                 end
             end
         end
@@ -173,4 +182,104 @@ for monk_no = 1:3
     colorbar
     set(plt,'AlphaData',~isnan(plot_mat))
     colormap jet
+    
+    figure
+    plot_mat = preferred_grating{monk_no};
+    plot_mat(find(isnan(plot_mat))) = -30;
+    plt = pcolor(1:10, 1:10, plot_mat);
+    title("Similar Preferred Orientations | Monk"+num2str(monk_no))
+    caxis([0, 330])
+    colorbar
+    shading interp
+    axis ij
+%     set(plt,'AlphaData',~isnan(plot_mat))
+    colormap jet
 end
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% part 3
+% finding dependence of r_sc on sitance
+
+% calculating tuning curves
+clc
+close all
+for monk_no = 1:3
+    neuron_indx = 1;
+    data = monk_data{monk_no};
+    neurons_info = monk_elec_info{monk_no};
+    neurons = 1:size(data(1).mean_FRs, 1);
+    activity_mat = zeros(length(neurons), 12);
+
+    for neuron_no = neurons
+        for grating_no = 1:12
+            data = monk_data{monk_no};
+            data = data(grating_no).mean_FRs;
+            activity = data(neuron_no, :);
+            activity = mean(activity, 2);
+            activity_mat(neuron_indx, grating_no) = activity/2e-2;    
+        end
+        neuron_indx = neuron_indx+1;
+    end
+    tuning_curves{monk_no} = activity_mat;
+end
+
+%% calculating r_s
+for monk_no = 1:3
+    tuning_curve_mat = tuning_curves{monk_no};
+    r_s_mat = zeros(size(tuning_curve_mat, 1));     
+    for neuron_1 = 1:size(tuning_curve_mat, 1)-1
+        for neuron_2 = neuron_1+1:size(tuning_curve_mat, 1)
+            tmp = corrcoef(tuning_curve_mat(neuron_1,:), tuning_curve_mat(neuron_2, :));
+            r_s_mat(neuron_1, neuron_2) = tmp(1,2); 
+        end
+    end
+    r_s{monk_no} = r_s_mat;
+end
+%% calculating r_sc
+for monk_no = 1:3
+    data =  monk_data{monk_no};
+    r_s_mat = r_s{monk_no};
+    r_sc_mat = zeros(size(r_s_mat, 1));
+    for neuron_1 = 1:size(r_s_mat, 1)-1
+        for neuron_2 = neuron_1+1:size(r_s_mat, 1)
+            firing_rate_mat = zeros(12*200, 2);
+            counter = 1;
+            for orientation_no = 1:12
+                for trial_no = 1:200
+                    counts = data(orientation_no).trial(trial_no).counts;
+                    firing_rate_mat(counter, 1) = sum(counts(neuron_1, :));
+                    firing_rate_mat(counter, 2) = sum(counts(neuron_2, :));
+                    counter = counter+1;
+                end
+            end
+            firing_rate_mat = zscore(firing_rate_mat);
+            tmp = corrcoef(firing_rate_mat);
+            r_sc_mat(neuron_1, neuron_2) = tmp(1, 2);
+        end
+    end
+    r_sc{monk_no} = r_sc_mat;
+end
+%% calculating distance between neurons
+distance_2_units = 400e-6;
+for monk_no = 1:3
+    data =  monk_data{monk_no};
+    neurons_info = monk_elec_info{monk_no};
+    loc_map = monk_data_stuff{monk_no}.MAP;
+    channels = monk_data_stuff{monk_no}.CHANNELS;
+    distance_mat = zeros(size(data(1).trial(1).spikes, 1));
+    for neuron_1 = 1:size(data(1).trial(1).spikes, 1)-1
+        for neuron_2 = neuron_1+1:size(data(1).trial(1).spikes, 1)
+            elec_1 = channels(neuron_1);
+            elec_2 = channels(neuron_2);
+            [row1, col1] = find(loc_map==elec_1);
+            [row2, col2] = find(loc_map==elec_2);
+            dx = distance_2_units*abs(row1-row2);
+            dy = distance_2_units*abs(col1-col2);
+            distance = sqrt(dx^2+dy^2);
+            distance_mat(neuron_1, neuron_2) = distance;
+        end
+    end
+    elec_distances{monk_no} = distance_mat;
+end
+%% plotting r_sc vs elec_distances
+
+
+
