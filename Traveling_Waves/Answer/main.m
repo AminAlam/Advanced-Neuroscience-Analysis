ca2367056300fa9b9e92c151e2ec39dd8bd0a13a
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-03-29 21:57:34 +0430

    stft and welch are added

--- Traveling_Waves/Answer/main.m
+++ Traveling_Waves/Answer/main.m
@@ -105,20 +105,21 @@ title('Averaged power spectrum over all trials of each channel')
 xlabel('Frequency (Hz)')
 ylabel('Power (dB)')
 xlim([0, 70])
-ylim([-40, 10])
+ylim([-35, 10])
 grid on
 
 figure
 plt = imagesc(dominant_freq_mat);
 set(plt,'AlphaData', ~isnan(dominant_freq_mat))
 colormap jet
-colorbar
+c_bar = colorbar;
 caxis([0, 13])
 title('Dominant Frequencies')
+ylabel(c_bar,'Frequency (Hz)')
 %% part c - time-frequncy analysis of the LFP data
 clc
 close all
-% STFT
+% STFT Method
 stft_map = 0;
 for ch_no = 1:num_channels
     lfp_data = chan(ch_no).lfp;
@@ -129,7 +130,7 @@ for ch_no = 1:num_channels
         stft_map = stft_map + abs(s); 
     end
 end
-%%
+
 stft_map_tmp = [];
 
 for t = 1:size(stft_map, 2)
@@ -137,29 +138,134 @@ for t = 1:size(stft_map, 2)
     n = length(Ps);
     Ps_plot = removePinkNoise(Ps, f', n);
     stft_map_tmp(:, t) = Ps_plot;
-%     figure
-%     plot(f(n/2+2:end), Ps(n/2+2:end), 'r')
-%     hold on
-%     plot(f(n/2+2:end), Ps_plot(n/2+2:end), '--b')
+end
+
+figure 
+imagesc(time_stft-1.2,f,stft_map/(num_channels*num_trials));
+ylim([0, 40])
+c_bar = colorbar;
+caxis([0, 13])
+set(gca,'YDir','normal')
+title("STFT")
+xlabel('Time (s)')
+ylabel('Frequency (Hz)')
+ylabel(c_bar,'Power (dB)')
+
+figure
+imagesc(time_stft-1.2,f(n/2+2:end),stft_map_tmp(n/2+2:end, :)/(num_channels*num_trials));
+ylim([2, 40])
+c_bar = colorbar;
+caxis([0, 13])
+set(gca,'YDir','normal')
+title("STFT - Denoised (no Pink noise)")
+xlabel('Time (s)')
+ylabel('Frequency (Hz)')
+ylabel(c_bar,'Power (dB)')
+%% Welch Method
+clc
+window_length = 80;
+num_overlap_samples = 60;
+pxx_mean = 0;
+for ch_no = 1:num_channels
+    lfp_data = chan(ch_no).lfp;
+    lfp_data = zscore(lfp_data);
+    for trial_no = 1:num_trials
+        trial_data = lfp_data(:, trial_no);
+        tmp = buffer(trial_data, window_length, num_overlap_samples);
+        [pxx,f] = pwelch(tmp, 40, 20, fs, fs);
+        pxx_mean = pxx_mean+pxx;
+    end
+end
+%%
+pxx_clean = [];
+for t = 1:size(pxx_mean, 2)
+    Ps = pxx_mean(:, t);
+    n = length(Ps);
+    Ps_plot = removePinkNoise(Ps, f', n, 2);
+    pxx_clean(:, t) = Ps_plot;
 end
 
 figure
-imagesc(time_stft-1.2,f,flipud(stft_map));
-ylim([0, 40])
-colorbar
+imagesc(linspace(-1.2, 2, size(tmp, 2)),f, pxx_mean/(num_channels*num_trials))
+c_bar = colorbar;
 set(gca,'YDir','normal')
+title("Power Spectrum over Time - Welch")
+xlabel('Time (s)')
+ylabel('Frequency (Hz)')
+ylabel(c_bar,'Power')
+caxis([-0.05, 0.1])
+ylim([0, 40])
 
 figure
-imagesc(time_stft-1.2,f,flipud(stft_map_tmp));
-ylim([0, 40])
-colorbar
+imagesc(linspace(-1.2, 2, size(tmp, 2)),f, pxx_clean/(num_channels*num_trials))
+c_bar = colorbar;
 set(gca,'YDir','normal')
+title("Power Spectrum over Time - Welch - Denoised (no Pink noise)")
+xlabel('Time (s)')
+ylabel('Frequency (Hz)')
+ylabel(c_bar,'Power')
+caxis([-0.05, 0.1])
+ylim([0, 40])
+%% Welch Method
+clc
+window_length = 80;
+num_overlap_samples = 60;
+pxx_mean = 0;
+for ch_no = 1:num_channels
+    lfp_data = chan(ch_no).lfp;
+    lfp_data = zscore(lfp_data);
+    for trial_no = 1:num_trials
+        trial_data = lfp_data(:, trial_no);
+        tmp = buffer(trial_data, window_length, num_overlap_samples);
+        [pxx,f] = pwelch(tmp, 40, 20, fs, fs);
+        pxx_mean = pxx_mean+pxx;
+    end
+end
+%%
+pxx_clean = [];
+for t = 1:size(pxx_mean, 2)
+    Ps = pxx_mean(:, t);
+    n = length(Ps);
+    Ps_plot = removePinkNoise(Ps, f', n, 2);
+    pxx_clean(:, t) = Ps_plot;
+end
+
+figure
+imagesc(linspace(-1.2, 2, size(tmp, 2)),f, pxx_mean/(num_channels*num_trials))
+c_bar = colorbar;
+set(gca,'YDir','normal')
+title("Power Spectrum over Time - Welch")
+xlabel('Time (s)')
+ylabel('Frequency (Hz)')
+ylabel(c_bar,'Power')
+% caxis([-0.05, 0.1])
+ylim([0, 40])
+
+figure
+imagesc(linspace(-1.2, 2, size(tmp, 2)),f, pxx_clean/(num_channels*num_trials))
+c_bar = colorbar;
+set(gca,'YDir','normal')
+title("Power Spectrum over Time - Welch - Denoised (no Pink noise)")
+xlabel('Time (s)')
+ylabel('Frequency (Hz)')
+ylabel(c_bar,'Power')
+% caxis([-0.05, 0.1])
+ylim([0, 40])
 %% Functions
 
-function Ps_plot = removePinkNoise(Ps, f, n)
-    pink_noise = 1./f(n/2+2:end);
-    [~,~,spectrum_regressed] = regress(Ps(n/2+2:end), pink_noise');
-    pink_spectrum = Ps(n/2+2:end)-spectrum_regressed;
-    Ps_plot = Ps;
-    Ps_plot(n/2+2:end) = Ps(n/2+2:end)-pink_spectrum;
+function Ps_plot = removePinkNoise(Ps, f, n, bool)
+    if bool==1
+        pink_noise = 1./f(n/2+2:end);
+        [~,~,spectrum_regressed] = regress(Ps(n/2+2:end), pink_noise');
+        pink_spectrum = Ps(n/2+2:end)-spectrum_regressed;
+        Ps_plot = Ps;
+        Ps_plot(n/2+2:end) = Ps(n/2+2:end)-pink_spectrum;
+    else
+        pink_noise = 1./f;
+        if pink_noise(1)==Inf, pink_noise(1)=pink_noise(2); end
+        [~,~,spectrum_regressed] = regress(Ps, pink_noise');
+        pink_spectrum = Ps-spectrum_regressed;
+        Ps_plot = Ps;
+        Ps_plot = Ps-pink_spectrum;
+    end
 end
