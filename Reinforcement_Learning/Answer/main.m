e2723f89c375118c1392f482dbae1da80baff375
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-05-10 23:46:22 +0430

    travelled path is plotted now

--- Reinforcement_Learning/Answer/main.m
+++ Reinforcement_Learning/Answer/main.m
@@ -10,15 +10,18 @@ target_img = imread('assets/target.png');
 % configs
 map_size = [15, 15];
 cat_loc = [ceil(map_size(1,1)*rand(1,1)), ceil(map_size(1,2)*rand(1,1))];
+cat_loc = [5, 8];
 target_loc = [ceil(map_size(1,1)*rand(1,1)), ceil(map_size(1,2)*rand(1,1))];
+target_loc = [9, 11];
 
 map_mat_main = zeros(map_size);
 map_mat_main(cat_loc(1,1), cat_loc(1,2)) = -1;
 map_mat_main(target_loc(1,1), target_loc(1,2)) = 1;
 
 num_trials = 100;
-learning_rate = 0.1;
+learning_rate = 0.01;
 num_directions = 4;
+
 direction_map{1} = [0, 1]; % up
 direction_map{2} = [0, -1]; % down
 direction_map{3} = [-1, 0]; % left
@@ -38,12 +41,14 @@ end
 
 softmax_func = @(x) exp(x)/sum(exp(x));
 figure('units','normalized','outerposition',[0 0 1 1])
+
 for trial_no = 1:num_trials
     reach_target_bool = 0;
     reach_cat_bool = 0;
     agent_loc = [ceil(map_size(1,1)*rand(1,1)), ceil(map_size(1,2)*rand(1,1))];
     step_no = 1;
-
+    agent_locs = [];
+    
     while ~reach_target_bool && ~reach_cat_bool
         agent_loc_past = agent_loc;
 
@@ -60,7 +65,8 @@ for trial_no = 1:num_trials
         if check_mat(1,1) > 0 && check_mat(1,2) > 0 && check_mat(1,1) < 15 && check_mat(1,2) < 15
            agent_loc = agent_loc + direction;
         else
-            for i = 1:num_directions
+            while 1
+                i = ceil(rand(1,1)*num_directions);
                 direction = direction_map{i};
                 check_mat = map_size - (agent_loc + direction);
                 if check_mat(1,1) > 0 && check_mat(1,2) > 0 && check_mat(1,1) < 15 && check_mat(1,2) < 15
@@ -77,10 +83,12 @@ for trial_no = 1:num_trials
         if agent_loc == cat_loc
             reach_cat_bool = 1;
         end
-        
+        agent_locs = [agent_locs; agent_loc];
         [states, transitions] = reach_target(states, transitions, direction_no, learning_rate, agent_loc, agent_loc_past);
+        
         subplot(2,3,[1,2,4,5])
-        plot_map(agent_loc, target_loc, cat_loc, map_size, rat_img, cat_img, target_img)
+        plot_map(agent_loc, agent_locs, target_loc, cat_loc, map_size, rat_img, cat_img, target_img)
+        title("Trial "+num2str(trial_no)+" | "+"Step "+num2str(step_no))
         subplot(2,3,3)
         imagesc(states);
         set(gca,'YDir','normal')
@@ -91,5 +99,6 @@ for trial_no = 1:num_trials
         quiver(fx,fy)
         step_no = step_no + 1;
         pause(0.05)
+        
     end
 end
--- Reinforcement_Learning/Answer/plot_map.m
+++ Reinforcement_Learning/Answer/plot_map.m
@@ -1,4 +1,4 @@
-function plot_map(agent_loc, target_loc, cat_loc, map_size, rat_img, cat_img, target_img)
+function plot_map(agent_loc, agent_locs, target_loc, cat_loc, map_size, rat_img, cat_img, target_img)
     scatter(agent_loc(1,2), agent_loc(1,1), 'k', 'filled')
     hold on
     image([agent_loc(1,2)-0.5, agent_loc(1,2)+0.5], [agent_loc(1,1)-0.5, agent_loc(1,1)+0.5], rat_img)
@@ -8,7 +8,12 @@ function plot_map(agent_loc, target_loc, cat_loc, map_size, rat_img, cat_img, ta
     
     scatter(cat_loc(1,2), cat_loc(1,1), 'r', 'filled')
     image([cat_loc(1,2)-0.5, cat_loc(1,2)+0.5], [cat_loc(1,1)-0.5, cat_loc(1,1)+0.5], cat_img)
-    
+    if size(agent_locs, 1)>2  
+        for i = 2:size(agent_locs, 1)
+            plot([agent_locs(i-1,2), agent_locs(i,2)], [agent_locs(i-1,1), agent_locs(i,1)], 'k')
+        end
+    end
+    scatter(agent_locs(1,2), agent_locs(1,1), 'k', 'filled')
     hold off
     xlim([0, map_size(1,1)+1])
     ylim([0, map_size(1,2)+1])
