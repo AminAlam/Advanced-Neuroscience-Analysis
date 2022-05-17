830356c2a73cf8458d51b5121fc0ddf51de76bec
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-05-17 17:48:14 +0430

    effect of parameters in TD rule added to the report

--- Reinforcement_Learning/Answer/main.m
+++ Reinforcement_Learning/Answer/main.m
@@ -171,7 +171,6 @@ end
 %% Step numbers vs trials
 
 clc
-clear
 close all
 
 save_figures = 1;
@@ -204,7 +203,7 @@ direction_map{2} = [0, -1]; % down
 direction_map{3} = [-1, 0]; % left
 direction_map{4} = [1, 0]; % right
 
-num_steps = zeros(num_iters, num_trials);
+num_steps_rl = zeros(num_iters, num_trials);
 
 for iter_no = 1:num_iters
     iter_no
@@ -285,11 +284,11 @@ for iter_no = 1:num_iters
                 break
             end
         end
-        num_steps(iter_no, trial_no) = step_no;
+        num_steps_rl(iter_no, trial_no) = step_no;
     end
 end
 
-plot(1:trial_step:num_trials, mean(num_steps(:, 1:trial_step:num_trials), 1), 'k', 'LineWidth', 1)
+plot(1:trial_step:num_trials, mean(num_steps_rl(:, 1:trial_step:num_trials), 1), 'k', 'LineWidth', 1)
 xlabel('Trial No')
 ylabel('Num of Steps')
 title('Num of steps to reach target vs Trials')
@@ -758,14 +757,13 @@ end
 
 clc
 clear
-close all
 
 save_figures = 1;
 
 % configs
 target_value = 2;
 cat_value = -2;
-num_trials = 500;
+num_trials = 1001;
 trial_step = 2;
 num_iters = 100;
 learning_rate_main = 0.1;
@@ -886,4 +884,169 @@ xlim([1, num_trials])
 if save_figures
     set(gcf,'PaperPositionMode','auto')
     print("Report/images/Q05",'-dpng','-r0')
+end
+
+%% TD vs simple RL
+clc
+close all
+save_figures = 1;
+figure
+plot(1:trial_step:num_trials, mean(num_steps(:, 1:trial_step:num_trials), 1), 'k', 'LineWidth', 1)
+hold on
+plot(1:trial_step:num_trials, mean(num_steps_rl(:, 1:trial_step:num_trials), 1), 'b', 'LineWidth', 1)
+xlabel('Trial No')
+ylabel('Num of Steps')
+legend('TD rule', 'Simple RL')
+title('Num of steps to reach target vs Trials')
+xlim([1, num_trials])
+
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q05_2",'-dpng','-r0')
+end
+
+
+%% Effect of learning factor and discount factor - TD Rule
+clc
+clear
+close all
+
+save_figures = 1;
+
+learning_rates = 0.1:0.3:1;
+discount_factors = 0.1:0.3:1;
+
+% configs
+target_value = 2;
+cat_value = -2;
+num_trials = 400;
+num_iters = 10;
+forgetting_factor = 1;
+max_steps = 200;
+
+map_size = [15, 15];
+cat_loc = [12, 13];
+target_loc = [7, 7];
+
+map_mat_main = zeros(map_size);
+map_mat_main(cat_loc(1,1), cat_loc(1,2)) = -1;
+map_mat_main(target_loc(1,1), target_loc(1,2)) = 1;
+
+num_directions = 4;
+
+direction_map{1} = [0, 1]; % up
+direction_map{2} = [0, -1]; % down
+direction_map{3} = [-1, 0]; % left
+direction_map{4} = [1, 0]; % right
+
+steps_mat = zeros(length(learning_rates), length(discount_factors));
+
+num_steps = zeros(num_iters, num_trials);
+lr_index = 0;
+
+for learning_rate = learning_rates
+    learning_rate
+    lr_index = lr_index+1;
+    df_index = 0;
+    for discount_factor = discount_factors
+        discount_factor
+        df_index = df_index+1;
+        for iter_no = 1:num_iters
+            % initializing the movement direction probabilities
+            states = zeros(map_size);
+
+            for i = 1:map_size(1,1)
+                for j = 1:map_size(1,2)
+                    transitions{i,j} = zeros(1,4);
+                end
+            end
+
+            % defining the softmax function
+            softmax_func = @(x) exp(x)/sum(exp(x));
+
+
+            states_r = zeros(map_size);
+            states_r(target_loc(1,1), target_loc(1,2)) = target_value;
+            states_r(cat_loc(1,1), cat_loc(1,2)) = cat_value;
+
+            for trial_no = 1:num_trials
+                % applying the forgeting factor
+                states = states*forgetting_factor;
+
+                reach_target_bool = 0;
+                reach_cat_bool = 0;
+                agent_loc = [randi(map_size(1,1), 1, 1), randi(map_size(1,2), 1, 1)];
+                agent_loc_past = [1, 1];
+                step_no = 1;
+                agent_locs = [agent_loc_past];
+                direction_nos = [1]; 
+
+                while ~reach_target_bool && ~reach_cat_bool
+
+
+                    % deciding which direction to choose
+                    directions_probs = softmax_func(cell2mat(transitions(agent_loc(1,1), agent_loc(1,2))));
+                    direction_no = choose_by_prob(directions_probs);
+
+                    direction = direction_map{direction_no};
+                    check_mat = map_size - (agent_loc + direction);
+                    if check_mat(1,1) > 0 && check_mat(1,2) > 0 && check_mat(1,1) < map_size(1,1) && check_mat(1,2) < map_size(1,2)
+                       agent_loc = agent_loc + direction;
+                    else
+                        for i = randperm(num_directions)
+                            direction = direction_map{i};
+                            check_mat = map_size - (agent_loc + direction);
+                            if check_mat(1,1) > 0 && check_mat(1,2) > 0 && check_mat(1,1) < map_size(1,1) && check_mat(1,2) < map_size(1,2)
+                                agent_loc = agent_loc + direction;
+                                pass_bool = 1;
+                                break
+                            end
+                        end
+                        if ~pass_bool
+                            agent_loc = [randi(map_size(1,1), 1, 1), randi(map_size(1,2), 1, 1)];
+                        end
+                    end
+
+                    % checking location
+
+                    agent_locs = [agent_locs; agent_loc];
+
+                    direction_nos = [direction_nos; direction_no];
+                    [states, transitions] = update_state_and_transition_TD(states_r, states, transitions, direction_nos, learning_rate, discount_factor, target_value, cat_value, softmax_func, agent_locs);
+            
+
+                    if agent_loc == target_loc
+                        reach_target_bool = 1;
+                        states(target_loc(1,1), target_loc(1,2)) = target_value;
+                    end
+                    if agent_loc == cat_loc
+                        reach_cat_bool = 1;
+                        states(cat_loc(1,1), cat_loc(1,2)) = cat_value;
+                    end
+
+                    step_no = step_no + 1;
+                    agent_loc_past = agent_loc;
+
+                    if step_no>max_steps
+                        break
+                    end
+                end
+                num_steps(iter_no, trial_no) = step_no;
+            end
+        end
+        num_steps = mean(num_steps, 1);
+        steps_mat(lr_index, df_index) = mean(num_steps(end-50:end));
+    end
+end
+
+imagesc(learning_rates, discount_factors, steps_mat)
+set(gca,'YDir','normal')
+ylabel('Learning Rate')
+xlabel('Discount Factor')
+c = colorbar;
+colormap bone
+c.Label.String = 'Avg number of steps to reach the target';
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q05_3_2",'-dpng','-r0')
 end
