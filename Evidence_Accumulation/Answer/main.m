494eef3839d41df844463a012f49b7ce8985371d
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-05-19 23:31:19 +0430

    part 3 with theory part is added

--- Evidence_Accumulation/Answer/main.m
+++ Evidence_Accumulation/Answer/main.m
@@ -14,14 +14,17 @@ choices = zeros(1, num_iters);
 for iter = 1:num_iters
     [X(:, iter), choices(1, iter)] = simple_model(bias, sigma, dt, time_interval);
 end
+random_gen_data = normrnd(bias*time_interval(end), sigma*time_interval(end), size(X,1), size(X,2));
 figure
 histogram(X, 'Normalization', 'pdf')
+hold on 
+histogram(random_gen_data, 'Normalization', 'pdf')
 xlabel('X')
 ylabel('Probability Density')
 title("PDF of X | B = " + num2str(bias))
+legend('X', 'N(0, \sigma)')
 figure
 qqplot(reshape(X, [], 1))
-
 
 bias = 1;
 X = zeros(length(time_interval), num_iters);
@@ -29,19 +32,23 @@ choices = zeros(1, num_iters);
 for iter = 1:num_iters
     [X(:, iter), choices(1, iter)] = simple_model(bias, sigma, dt, time_interval);
 end
+random_gen_data = normrnd(bias*time_interval(end), sigma*time_interval(end), size(X,1), size(X,2));
 figure
 histogram(X, 'Normalization', 'pdf')
+hold on 
+histogram(random_gen_data, 'Normalization', 'pdf')
 xlabel('X')
 ylabel('Probability Density')
 title("PDF of X | B = " + num2str(bias))
+legend('X', 'N(1, \sigma)')
 figure
 qqplot(reshape(X, [], 1))
 
 figure
-plot(time_interval, mean(X, 2), 'k', 'LineWidth', 2)
+plot(time_interval, mean(X, 2), 'k', 'LineWidth', 3)
 hold on
 plot(time_interval, X', '--k')
-legend('Avg of all Iteratoins', 'Differnet Iterations', 'Location', 'northwest')
+legend('Avg of all Iteratoins', 'Different Iterations', 'Location', 'northwest')
 xlabel('Time (s)')
 ylabel('X')
 %% 10 second trial
@@ -73,9 +80,11 @@ num_iters = 1000;
 sigma = 1;
 dt = 0.1;
 bias = 0.1;
-times = 0.5:1:10;
+times = 0.5:1:50;
 error = zeros(1, length(times));
+error_theory = zeros(1, length(times));
 time_index = 1;
+
 for time = times
     time_interval = 0:dt:time;
     choices = zeros(1, num_iters);
@@ -84,10 +93,26 @@ for time = times
         [X(:, iter), choices(1, iter)] = simple_model(bias, sigma, dt, time_interval);
     end
     error(1, time_index) = 1-sum(choices == sign(bias))/length(choices);
+    
+    mean_theory = bias*time;
+    sigma_theory = sqrt(dt*time);
+    error_prob = 1 - min(8*sigma_theory, mean_theory+4*sigma_theory)/(8*sigma_theory);
+    error_theory(1, time_index) = error_prob;
+    
     time_index = time_index+1;
 end
 
 plot(times, error, 'k', 'LineWidth', 2)
+hold on
+plot(times, error_theory, 'r', 'LineWidth', 2)
 xlabel('Time Intervals')
 ylabel('Error (percantage of wrong choices)')
+legend('Simulation', 'Theory')
 ylim([0,0.6])
+%% plots for report - theory part proof
+
+
+
+
+
+
