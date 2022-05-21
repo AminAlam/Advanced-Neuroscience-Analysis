a7e254f861ae33787130eb21e916b26de99a7e0f
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-05-21 21:45:35 +0430

    LIP function is completed now

--- Evidence_Accumulation/Answer/lip_activity.m
+++ Evidence_Accumulation/Answer/lip_activity.m
@@ -1,23 +1,34 @@
-function rt = lip_activity(MT_p_values,LIP_weights,LIP_threshold)
+function [rt, LIP_event_times, MT, times] = lip_activity(MT_p_values, LIP_weights, LIP_threshold)
     % Parameters:
     % MT_p_values - a vector with 2 elements, firing probabilities for the
     % excitatory and inhibitory neurons, resp.
     % LIP_weights - a length 2 vector of weighting factors for the evidence
-    % from the excitatory (positive) and
-    % inhibitory (negative) neurons
-    % LIP_threshold - the LIP firing rate that represents the choice threshold criterion % use fixed time scale of 1 ms
+    % from the excitatory (positive) and inhibitory (negative) neurons
+    % LIP_threshold - the LIP firing rate that represents the choice threshold criterion \
+    % use fixed time scale of 1 ms
     dt = 0.001;
-    N = [0 0]; % plus is first, minus is second
+    N = [0; 0]; % plus is first, minus is second
     rate = 0.0;
     LIP_event_times = [];
+    t = 0;
+    M = 100;
+    MT = [];
+    times = [];
     while rate < LIP_threshold
-    t = t + 0.001;
-    dN = rand(2) < MT_p_values;
-    N = N + dN;
-    p_LIP = sum(N.*LIP_weights);
-    LIP_event = rand < p_LIP;
-    _event_times = [LIP_event_times t];
-    % check LIP mean rate for last M spikes rate = M/(t - LIP_event_times(N_LIP - M));
+        times = [t, times];
+        t = t + dt;
+        dN = rand(2, 1) < MT_p_values;
+        MT = [MT, dN];
+        N = N + dN;
+        p_LIP = LIP_weights'*N;
+        LIP_event = rand(1,1) < p_LIP;
+        if LIP_event
+            LIP_event_times = [LIP_event_times, t];
+        end
+        if  length(LIP_event_times)>M
+            rate = M/(t-LIP_event_times(end-M+1));
+        end
+        % check LIP mean rate for last M spikes rate = M/(t - LIP_event_times(N_LIP - M));
     end
     rt = t;
 end
--- Evidence_Accumulation/Answer/main.m
+++ Evidence_Accumulation/Answer/main.m
@@ -3,6 +3,8 @@ clc
 clear
 close all
 
+save_figures = 1;
+
 num_iters = 20;
 sigma = 1;
 dt = 0.1;
@@ -14,6 +16,7 @@ choices = zeros(1, num_iters);
 for iter = 1:num_iters
     [X(:, iter), choices(1, iter)] = simple_model(bias, sigma, dt, time_interval);
 end
+
 random_gen_data = normrnd(bias*time_interval(end), sigma*time_interval(end), size(X,1), size(X,2));
 figure
 histogram(X, 'Normalization', 'pdf')
@@ -23,8 +26,19 @@ xlabel('X')
 ylabel('Probability Density')
 title("PDF of X | B = " + num2str(bias))
 legend('X', 'N(0, \sigma)')
+
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/pdf_bias_0",'-dpng','-r0')
+end
+
 figure
 qqplot(reshape(X, [], 1))
+
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/qqplot_bias_0",'-dpng','-r0')
+end
 
 bias = 1;
 X = zeros(length(time_interval), num_iters);
@@ -41,8 +55,20 @@ xlabel('X')
 ylabel('Probability Density')
 title("PDF of X | B = " + num2str(bias))
 legend('X', 'N(1, \sigma)')
+
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/pdf_bias_1",'-dpng','-r0')
+end
+
 figure
 qqplot(reshape(X, [], 1))
+
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/qqplot_bias_1",'-dpng','-r0')
+end
+
 
 figure
 plot(time_interval, mean(X, 2), 'k', 'LineWidth', 3)
@@ -51,9 +77,12 @@ plot(time_interval, X', '--k')
 legend('Avg of all Iteratoins', 'Different Iterations', 'Location', 'northwest')
 xlabel('Time (s)')
 ylabel('X')
-%% 10 second trial
-clc
-close all
+
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/different_Xs_bias_1",'-dpng','-r0')
+end
+% 10 second trial
 
 sigma = 1;
 dt = 0.1;
@@ -66,12 +95,19 @@ for bias = biases
     [X(bias_index, :), ~] = simple_model(bias, sigma, dt, time_interval);
     bias_index = bias_index+1;
 end
-
-plot(time_interval, X, 'LineWidth', 2)
+figure
+hold on
+for i = 1:size(X, 1)
+    plot(time_interval, X(i,:), 'LineWidth', 2)
+end
 xlabel('Time (s)')
 ylabel('X')
 legend('B = -1', 'B = 0', 'B = 0.1', 'B = 1', 'B = 10', 'Location', 'northwest')
 
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/10sec_different_biases",'-dpng','-r0')
+end
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Q03 - Relatoin between Error rate and time
 clc
 close all
@@ -109,11 +145,14 @@ xlabel('Time Intervals')
 ylabel('Error (percantage of wrong choices)')
 legend('Simulation', 'Theory')
 ylim([0,0.6])
+
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Error",'-dpng','-r0')
+end
 %% plots for report - theory part proof
 clc
 close all
-
-save_figures = 1;
 
 x = -5:.1:5;
 y = normpdf(x,0,1);
@@ -185,8 +224,13 @@ xlabel('Time (s)')
 title('Variance of X(t)')
 legend('Simulation', 'Theory', 'Location', 'northwest')
 
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/X_mean_and_variance",'-dpng','-r0')
+end
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Q05 - Calculating the probability of right choice 
-%                                       using the CDF of normal distribution 
+%                                       using the CDF of normal distribution
+
 clc
 close all
 
@@ -223,6 +267,11 @@ c = colorbar('Ticks', [time_limits(1), time_limits(end/2), time_limits(end)], 'T
 c.Label.String = 'Time Limit (s)';
 caxis([time_limits(1), time_limits(end)])
 
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q05_plot",'-dpng','-r0')
+end
+
 figure
 imagesc(Xs_0, time_limits, probs)
 set(gca,'YDir','normal')
@@ -232,6 +281,10 @@ colormap bone
 c = colorbar;
 c.Label.String = 'Probability';
 
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q05_countor_plot",'-dpng','-r0')
+end
 
 % 3D plot for showing effect of bias
 biases = -0.2:0.1:0.2;
@@ -267,6 +320,11 @@ colormap bone
 c = colorbar;
 c.Label.String = 'Time Limit (s)';
 
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q05_3D",'-dpng','-r0')
+end
+
 for bias_index = 1:length(biases)
     figure
     for time_limit_index = 1:length(time_limits)
@@ -279,8 +337,13 @@ for bias_index = 1:length(biases)
     c = colorbar;
     c.Label.String = 'Time Limit (s)';
     title("Bias = "+num2str(biases(bias_index)))
-end
+    
+    if save_figures
+        set(gcf,'PaperPositionMode','auto')
+        print("Report/images/Q05_plot_"+num2str(bias_index),'-dpng','-r0')
+    end
 
+end
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Q07 - Simulating situation of free response
 clc
 close all
@@ -321,6 +384,12 @@ xlabel('Reaction Time')
 ylabel('Probability Density')
 legend('Simulation', 'Theory')
 
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q07_all",'-dpng','-r0')
+end
+
+
 figure
 hold on
 plot(hist_correct_bins/dt, hist_correct_values*dt, 'b', 'LineWidth', 2)
@@ -329,9 +398,14 @@ xlabel('Reaction Time')
 ylabel('Probability Density')
 legend('Correct choices', 'Wrong choices')
 
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q07_correct_and_wrong",'-dpng','-r0')
+end
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Q08 - Simulating situation of free response
 clc
 close all
+
 threshold = 10;
 biases = 1:1:10;
 sigma = 1;
@@ -361,6 +435,85 @@ colormap bone
 c = colorbar;
 c.Label.String = 'Win Rate of Choice no 1';
 
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q08",'-dpng','-r0')
+end
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Q09 - Simulating situation of free response with time limit
+clc
+close all
+
+threshold = 10;
+biases = 1:1:10;
+sigma = 1;
+X_0 = [0;0];
+dt = 0.01;
+num_iters = 1000;
+time_limit = 2;
+stats = zeros(length(biases), length(biases), num_iters);
+
+bias_1_index = 1;
+for bias_1 = biases
+    bias_2_index = 1;
+    for bias_2 = biases
+        bias = [bias_1; bias_2];
+        for iter_no = 1:num_iters
+            stats(bias_1_index, bias_2_index, iter_no)  = race_trial_with_time_limit(threshold, bias, sigma, X_0, dt, time_limit);
+        end
+        bias_2_index = bias_2_index+1;
+    end
+    bias_1_index = bias_1_index+1;
+end
+
+imagesc(biases, biases, 100*sum(stats==3, 3)/size(stats, 3))
+set(gca,'YDir','normal')
+xlabel('Bias of Choice 2')
+ylabel('Bias of Choice 1')
+colormap bone
+c = colorbar;
+c.Label.String = 'Percentage of trials when time exceeds the threshold';
+
+if save_figures
+    set(gcf,'PaperPositionMode','auto')
+    print("Report/images/Q09",'-dpng','-r0')
+end
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Part 2 - Q01 - event times of the modified lip neurons
+clc
+close all
+
+LIP_threshold = 50;
+MT_p_values = [0.1; 0.05];
+LIP_weights = [0.1; -0.2];
+[rt, lip_event_times, MT, times] = lip_activity(MT_p_values, LIP_weights, LIP_threshold);
+figure
+hold on
+scatter(lip_event_times, lip_event_times*0+3, 'k', 'filled')
+scatter(times(MT(1,:)==1), MT(1, MT(1,:)==1)*0+2, 'b', 'filled')
+scatter(times(MT(2,:)==1), MT(2, MT(2,:)==1)*0+1, 'r', 'filled')
+legend('LIP Event Times', 'MT Neuron excitatory', 'MT Neuron inhibitory')
+ylim([0, 4])
+xlim([lip_event_times(1)-0.01, lip_event_times(end)+0.01])
+xlabel('Time (s)')
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Part 2 - Q02 - MR neurons with prefered orientation 
+clc
+close all
+
+sigma = 0.4;
+stimuli_values = 0:0.01:6;
+Mt_neuron_1 = makedist('Normal','mu',2,'sigma',sigma);
+Mt_neuron_2 = makedist('Normal','mu',4,'sigma',sigma);
+figure
+hold on
+plot(stimuli_values, pdf(Mt_neuron_1, stimuli_values), 'LineWidth', 2)
+plot(stimuli_values, pdf(Mt_neuron_2, stimuli_values), 'LineWidth', 2)
+ylim([0, 1])
+xlabel('Stimuli Value')
+ylabel('Probability of Firing')
+legend('MT Neuron 1', 'MT Neuron 2')
+
+
+
 
 
