c32e0f34d0a164793e396ad48c6aa58f06e5ac60
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-06-10 17:33:46 +0430

    bird part completed

--- Visual_Model/Answer/main.m
+++ Visual_Model/Answer/main.m
@@ -23,10 +23,10 @@ for img_index = 1:size(IMAGESr, 3)
     IMAGES_cell{img_index} = img;
 end
 figure
-MONTAGE(IMAGES_cell_raw,'size',[2,5])
+montage(IMAGES_cell_raw,'size',[2,5])
 colormap gray
 figure
-MONTAGE(IMAGES_cell,'size',[2,5])
+montage(IMAGES_cell,'size',[2,5])
 colormap gray
 
 % showing basis functions
@@ -77,10 +77,10 @@ for img_index = 1:size(IMAGESr, 3)
     IMAGES_cell{img_index} = img;
 end
 figure
-MONTAGE(IMAGES_cell_raw,'size',[2,5])
+montage(IMAGES_cell_raw,'size',[2,5])
 colormap gray
 figure
-MONTAGE(IMAGES_cell,'size',[2,5])
+montage(IMAGES_cell,'size',[2,5])
 colormap gray
 
 % showing basis functions
@@ -121,10 +121,10 @@ for img_index = 1:size(IMAGESr, 3)
     IMAGES_cell{img_index} = img;
 end
 figure
-MONTAGE(IMAGES_cell_raw,'size',[2,5])
+montage(IMAGES_cell_raw,'size',[2,5])
 colormap gray
 figure
-MONTAGE(IMAGES_cell,'size',[2,5])
+montage(IMAGES_cell,'size',[2,5])
 colormap gray
 
 % showing basis functions
@@ -172,10 +172,10 @@ for img_index = 1:size(IMAGESr, 3)
     IMAGES_cell{img_index} = img;
 end
 figure
-MONTAGE(IMAGES_cell_raw,'size',[2,5])
+montage(IMAGES_cell_raw,'size',[2,5])
 colormap gray
 figure
-MONTAGE(IMAGES_cell,'size',[2,5])
+montage(IMAGES_cell,'size',[2,5])
 colormap gray
 
 % showing basis functions
@@ -198,10 +198,11 @@ for frame_no = 1:size(frames, 4)
     frame = frames(:,:,:,frame_no);
     frame = rgb2gray(frame);
     frame = frame(:, 33:end-32);
+    frames_n(:,:,:,frame_no) = frames(:, 33:end-32,:,frame_no);
     frame = whitener(frame);
     frames_prep(:,:,frame_no) = frame;
 end
-
+frames = frames_n;
 
 % showing the frames of the video
 for frame_no = 1:10
@@ -214,13 +215,15 @@ for frame_no = 1:10
 end
 
 figure
-MONTAGE(IMAGES_cell_raw,'size',[2,5])
+montage(IMAGES_cell_raw,'size',[2,5])
 colormap gray
 figure
-MONTAGE(IMAGES_cell,'size',[2,5])
+montage(IMAGES_cell,'size',[2,5])
 colormap gray
 
-% learning the basis functions
+%% learning the basis functions
+clc
+close all
 
 IMAGES = frames_prep(:,:,1:10);
 
@@ -229,8 +232,10 @@ A = A*diag(1./sqrt(sum(A.*A)));
 figure(1), colormap(gray)
 sparsenet
 
-%%
+%% calculating coefficients for each frame
 clc
+close all
+
 imSz = size(frames_prep,[1 2]);
 patchSz = [sz sz];
 xIdxs = [1:patchSz(2):imSz(2) imSz(2)+1];
@@ -238,13 +243,13 @@ yIdxs = [1:patchSz(1):imSz(1) imSz(1)+1];
 
 batch_size = (length(yIdxs)-1)^2;
 
-S_all = {};
-figure
+S_all = zeros(size(A,2), batch_size, size(frames, 3));
+
 for frame_no = 11:size(frames_prep, 3)
     I = frames_prep(:,:,frame_no);
     X = zeros(L,batch_size);
-    patch_count = 1;
     
+    patch_count = 1;
     for i = 1:length(yIdxs)-1
         Isub = I(yIdxs(i):yIdxs(i+1)-1,:);
         for j = 1:length(xIdxs)-1
@@ -254,27 +259,143 @@ for frame_no = 11:size(frames_prep, 3)
     end
 
     S = cgf_fitS(A,X,noise_var,beta,sigma,tol);
-    S_all{frame_no} = S;
-    [~,max_coeffs] = max(S);
-    textStrings = num2str(max_coeffs');
-    textStrings = strtrim(cellstr(textStrings));
-    max_coeffs_vector = max_coeffs;
-    max_coeffs = reshape(max_coeffs, [sqrt(batch_size), sqrt(batch_size)]);
+    S_all(:,:,frame_no) = S;
+end
 
-    imagesc(max_coeffs)
+%% plotting the basis function which has the bigesst coeff for each of the patchs
+clc
+close all
+figure(1)
+figure(2)
+
+[L M]=size(A);
+sz=sqrt(L);
+buf=1;
+if floor(sqrt(M))^2 ~= M
+  m=sqrt(M/2);
+  n=M/m;
+else
+  m=sqrt(M);
+  n=m;
+end
+
+for frame_no = 11:size(frames_prep, 3)
+    frame_no
+    S = S_all(:,:,frame_no);
+    array_all=-ones(1+m*(sz+1),1+n*(sz+1));
+    [max_coeffs_all, max_coeffs_index_all] = maxk(S, 100);
+        
+    for max_k = 30
+        array=-ones(1+m*(sz+1),1+n*(sz+1));
+        max_coeffs_index = max_coeffs_index_all(max_k,:);
+        max_coeffs = max_coeffs_all(max_k,:);
+        max_coeffs_index_vector = max_coeffs_index;
+        max_coeffs_vector = max_coeffs;
+
+        basis_function_map = {};
+        for i = 1:length(max_coeffs_index_vector)
+            basis_function = return_basis_functions(A,S,max_coeffs_index_vector(i));
+            basis_function_map{i} = basis_function*max_coeffs_vector(i);
+        end
+
+        k=1;
+        for i=1:m
+            for j=1:n
+                array(buf+(i-1)*(sz+buf)+[1:sz],buf+(j-1)*(sz+buf)+[1:sz])=...
+                basis_function_map{k};
+                k=k+1;
+            end
+        end
+        array_all = array_all+array;
+    end    
+    
+    figure(1)
+    img_imagesc = imagesc(array_all);
     set(gca,'YDir','normal')
-
-    [x, y] = meshgrid(1:sqrt(batch_size), 1:sqrt(batch_size));  
-    hStrings = text(x(:), y(:), textStrings(:), ...  
-                    'HorizontalAlignment', 'center');
-    midValue = mean(get(gca, 'CLim'));  
-    textColors = repmat(max_coeffs_vector(:) > midValue, 1, 3);  
-    textColors = ~textColors;
-    set(hStrings, {'Color'}, num2cell(textColors, 2));
     caxis([1, size(A,2)])
-    colormap gray
-    colorbar
+    img_imagesc = img_imagesc.CData;
+    img_imagesc = imresize(img_imagesc, [288, 288]);
+
+    img_main = frames(:,:,:,frame_no);
+    image(img_main);
+    
+    hold on
+    img_imagesc = imagesc(img_imagesc);
+    img_imagesc.AlphaData = 0.4;
+    colormap jet
+    caxis([-2, 2])
+    hold off
+    
+    figure(2)
+    imagesc(array_all);
+    colormap jet
+    caxis([-2, 2])
     pause(0.5)
 end
 
+%% patch plots
+clc
+close all
+
+[L M]=size(A);
+sz=sqrt(L);
+buf=1;
+if floor(sqrt(M))^2 ~= M
+  m=sqrt(M/2);
+  n=M/m;
+else
+  m=sqrt(M);
+  n=m;
+end
+
+sudden_moves = [38, 60, 80, 88];
+patchs = [1, 15, 30, 45, 80, 100];
+for patch_no = patchs
+    figure
+    for sudden_move = sudden_moves
+        xline(sudden_move, '--k');
+    end
+    for basis_no = 1:20
+        hold on
+        basis_values = S_all(basis_no, patch_no, 11:end);
+        plot(11:size(frames,4), squeeze(basis_no*2+basis_values), 'LineWidth', 2)
+    end
+    xlim([11, size(frames,4)])
+    xlabel('Frames')
+    ylabel('Basis Functions')
+    title("Patch No."+num2str(patch_no))
+    legend('Sudden Moves')
+    
+    figure
+    array=zeros(1+m*(sz+1),1+n*(sz+1));
+    k=1;
+    for i=1:m
+        for j=1:n
+            if k == patch_no
+                array(buf+(i-1)*(sz+buf)+[1:sz],buf+(j-1)*(sz+buf)+[1:sz])=1+...
+                    array(buf+(i-1)*(sz+buf)+[1:sz],buf+(j-1)*(sz+buf)+[1:sz]);
+            end
+            k=k+1;
+        end
+    end
+    
+    img_imagesc = imagesc(array);
+    set(gca,'YDir','normal')
+    caxis([0, 1])
+    img_imagesc = img_imagesc.CData;
+    img_imagesc = imresize(img_imagesc, [288, 288]);
+
+    img_main = frames(:,:,:,1);
+    image(img_main);
+    
+    hold on
+    img_imagesc = imagesc(img_imagesc);
+    img_imagesc.AlphaData = 0.5;
+    hold off 
+end
+
+%% %%%%%%%%%%%%%%% the role of attention models in the basis functions
+addpath 'gbvs/'
+clc
+close all
 
